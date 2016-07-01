90d8bc2f092f94faf39bbf3513805cc4c2dec80a
Konstiantyn Chernovol <Konstiantyn.Chernovol@playtech.com>
2016-07-01 16:15:39 +0300

    gradle aded

--- src/test/java/akhokor/DictionaryCheckerUtilTest.java
+++ src/test/java/akhokor/DictionaryCheckerUtilTest.java
@@ -1,19 +1,11 @@
-package test.akhokor;
+package akhokor;
 
-import junit.framework.TestCase;
-import main.akhokor.DictionaryCheckerUtil;
-import main.akhokor.bohr.Bohr;
-import main.akhokor.model.Node;
-import org.junit.Test;
-import org.junit.runners.model.TestClass;
+import  akhokor.
 
-/**
- * Created by kostiantync on 6/30/2016.
- */
 public class DictionaryCheckerUtilTest extends TestCase{
     @Test
     public void testGetWordByLeafNode() {
-        Node node = new Node(null);
+        main.akhokor.model.Node node = new Node(null);
         Node leafNode = new Node(node,'a',true);
         node.addNode(leafNode);
         Bohr bohr = new Bohr(node);
